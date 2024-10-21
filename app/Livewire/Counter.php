923482553f0427c6931a2cd70acd5ede73f59dea
Sacha Meunier <sacha.meunier@student.hepl.be>
2024-10-21 10:13:17 +0200

    Update counter using Alpine
    Updated the counter to use Alpine instead of Livewire, this means there is no request to the server.
    
    The class can be empty with Alpine.

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -6,20 +6,5 @@ use Livewire\Component;
 
 class Counter extends Component
 {
-    public $counter;
-
-    public function increment(): void
-    {
-        $this->counter++;
-    }
-
-    public function decrement(): void
-    {
-        $this->counter--;
-    }
-
-    public function mount()
-    {
-        $this->counter = 0;
-    }
+    
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,13 @@
-<div>
-    <button wire:click="decrement">-</button>
-    <span>{{ $counter }}</span>
-    <button wire:click="increment">+</button>
+<div x-data="{
+    counter:0,
+    decrement(){
+        this.counter--
+    },
+    increment(){
+        this.counter++
+    },
+}">
+    <button @click="decrement">-</button>
+    <span x-html="counter"></span>
+    <button @click="increment">+</button>
 </div>
